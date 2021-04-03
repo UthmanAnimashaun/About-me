bd5f327e70bca50a2dbdb9ea3d8959831b7bd39f
UthmanAnimashaun <animashaunua@gmail.com>
2021-04-03 16:38:27 +0100

    compare changes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-function openCity(evt, cityName){
-    var i, tabcontent, tablinks;
-
-    // to hide the tab content with class="tabcontent
-    tabcontent= document.getElementsByClassName("tabcontent");
-    for(i=0; i<tabcontent.length; i++){
-        tabcontent[i].style.display="none";
-    }
-
-    tablinks= document.getElementsByClassName("tablinks");
-    for(i=0; i<tablinks.length; i++){
-        tablinks[i].className=tablinks[i].className.replace(" active", "");
-
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display ="block";
-    evt.currentTarget.className +=" active";
-
-}
-=======
 function openCity(evt, cityName){
     var i, tabcontent, tablinks;
 
@@ -43,4 +21,3 @@ function openCity(evt, cityName){
 
 
 
->>>>>>> Stashed changes
