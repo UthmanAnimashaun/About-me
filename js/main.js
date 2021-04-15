2910b0cb77855b21b840a953dc204623cc8f514d
UthmanAnimashaun <animashaunua@gmail.com>
2021-04-15 12:07:01 +0100

    UI edited
    I edited the UI
    and change Lewis music to Katie
    added some comment to main js
    styled the css

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
+// -------------------- Tab Link/Menu Starts here---------------
 function openCity(evt, cityName){
     var i, tabcontent, tablinks;
-
+    
     // to hide the tab content with class="tabcontent
     tabcontent= document.getElementsByClassName("tabcontent");
     for(i=0; i<tabcontent.length; i++){
         tabcontent[i].style.display="none";
     }
-
+    
     tablinks= document.getElementsByClassName("tablinks");
     for(i=0; i<tablinks.length; i++){
         tablinks[i].className=tablinks[i].className.replace(" active", "");
-
+        
     }
-
+    
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display ="block";
     evt.currentTarget.className +=" active";
 }
-// Dark Mode
+// -------------------- Tab Link/Menu ends here---------------
+
+// -----------------------Dark Mode Starts here-----------------
 
 var app = document.getElementsByTagName("BODY")[0];
 // to make it stay either in light mode or darkmode.
-    if(localStorage.lightMode=="dark"){
-        app.setAttribute("darkmode","dark");
+if(localStorage.lightMode=="dark"){
+    app.setAttribute("darkmode","dark");
         document.getElementById("switch").checked=true;
     }
 function darkModes(){
@@ -35,6 +38,4 @@ function darkModes(){
     }
     console.log("lightmode="+localStorage.lightMode);
 }
-
-
-
+// -----------------------Dark Mode endss here-----------------
